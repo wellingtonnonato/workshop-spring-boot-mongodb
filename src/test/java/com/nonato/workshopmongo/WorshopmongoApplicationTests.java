0c446a1a2cde2fb0352f2512555af9667c412891
Wellington Nonato <wellingtonnonato@gmail.com>
2025-10-01 07:37:28 -0300

    Primeiro commit projeto criado

---
+++ src/test/java/com/nonato/workshopmongo/WorshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonato.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
