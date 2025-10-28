4d290161de4f8ba881ea881cd02ae9b1470834ec
Wellington Nonato <wellingtonnonato@gmail.com>
2025-10-28 08:02:11 -0300

    OInerção de usuário com POST

--- src/main/java/com/nonato/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nonato/workshopmongo/resources/UserResource.java
@@ -2,14 +2,14 @@ package com.nonato.workshopmongo.resources;
 
 import com.nonato.workshopmongo.domain.User;
 import com.nonato.workshopmongo.dto.UserDTO;
+import jakarta.servlet.Servlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.nonato.workshopmongo.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +31,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/nonato/workshopmongo/services/UserService.java
+++ src/main/java/com/nonato/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.nonato.workshopmongo.services;
 
 import com.nonato.workshopmongo.domain.User;
+import com.nonato.workshopmongo.dto.UserDTO;
 import com.nonato.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
