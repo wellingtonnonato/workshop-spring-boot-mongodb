20eb62246fe03f8a536131edc9719ae4b9c21752
Wellington Nonato <wellingtonnonato@gmail.com>
2025-11-04 07:09:07 -0300

    VaDeção de usuário com DELETE

--- src/main/java/com/nonato/workshopmongo/services/UserService.java
+++ src/main/java/com/nonato/workshopmongo/services/UserService.java
@@ -30,6 +30,12 @@ public class UserService {
     }
 
     public User fromDTO(UserDTO objDto){
+
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
+    public void delete(String id){
+        findById(id);
+        repo.deleteById(id);
     }
 }
