31fbff63429dcc6f5b75550186f1647afc53a8e2
Wellington Nonato <wellingtonnonato@gmail.com>
2025-11-04 07:54:03 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/nonato/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.nonato.workshopmongo.repository;
+
+import com.nonato.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
