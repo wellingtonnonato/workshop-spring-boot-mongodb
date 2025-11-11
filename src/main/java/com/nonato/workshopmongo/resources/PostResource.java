00607d1bbb7fc01121ff8e40a1b35fe7f92cbf03
Wellington Nonato <wellingtonnonato@gmail.com>
2025-11-11 19:46:12 -0300

    Consulta simples com @Query

--- src/main/java/com/nonato/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nonato/workshopmongo/resources/PostResource.java
@@ -29,4 +29,11 @@ public class PostResource {
         return ResponseEntity.ok().body(list);
     }
 
+    @GetMapping(value = "/searchTitle")
+    public ResponseEntity<List<Post>> searchTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.searchTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
