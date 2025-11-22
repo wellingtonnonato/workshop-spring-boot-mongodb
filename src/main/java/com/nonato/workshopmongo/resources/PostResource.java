7d217a5c23b92ba284bbe92a988a81777c24b4cc
Wellington Nonato <wellingtonnonato@gmail.com>
2025-11-22 15:44:08 -0300

    Consulta com vários critérios

--- src/main/java/com/nonato/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nonato/workshopmongo/resources/PostResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -33,6 +34,18 @@ public class PostResource {
     public ResponseEntity<List<Post>> searchTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> list = service.searchTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(minDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 
--- src/main/java/com/nonato/workshopmongo/services/PostService.java
+++ src/main/java/com/nonato/workshopmongo/services/PostService.java
@@ -9,6 +9,7 @@ import com.nonato.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,4 +31,10 @@ public class PostService {
     public List<Post> searchTitle(String text){
         return repo.searchTitle(text);
     }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + (24 * 60 * 60 * 1000));
+        return repo.fullSearch(text, minDate, maxDate);
+    }
+
 }
